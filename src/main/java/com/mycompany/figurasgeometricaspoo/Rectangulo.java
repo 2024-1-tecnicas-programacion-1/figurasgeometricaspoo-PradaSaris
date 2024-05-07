bf5ca2f84014c0f1c6aa1265cc3f31418d65b205
PradaSaris <159457017+PradaSaris@users.noreply.github.com>
2024-05-07 00:33:51 -0500

    Update Rectangulo.java

--- src/main/java/com/mycompany/figurasgeometricaspoo/Rectangulo.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/Rectangulo.java
@@ -1,5 +1,24 @@
 package com.mycompany.figurasgeometricaspoo;
 
-public class Rectangulo {
+public class Rectangulo extends FiguraGeometrica {
+    private double lado1;
+    private double lado2;
     
+    public Rectangulo (String nombre, String color, double lado1, double lado2){
+        super(nombre, color);
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+    
+    @Override
+    public double obtenerArea() {
+        double areaRectan = lado1*lado2;
+        return areaRectang;
+    }
+    
+    @Override
+    public double obtenerPerimetro() {
+        double perímetroRectang = (2*lado1) + (2*lado2);
+        return perimetroRectang;
+    } 
 }
